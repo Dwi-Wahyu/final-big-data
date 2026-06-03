38535c720e5df34df5e3aef4b6748759b4e6a279
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-06-03 19:07:35 +0800

    fix script

--- balance-computing.sh
+++ balance-computing.sh
@@ -1,3 +1,1 @@
-podman exec -it spark-master spark-submit \
-  --master spark://192.168.x.x:7077 \
-  /app/2_spark_streaming.py
+podman exec -it spark-master spark-submit --master spark://192.168.x.x:7077 /app/2_spark_streaming.py
