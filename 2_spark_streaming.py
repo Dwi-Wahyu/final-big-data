9b25f58c038eae6e44a687e043dec5f8ae100344
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-06-04 12:01:44 +0800

    update comments for clarity and organization in Spark streaming script

--- 2_spark_streaming.py
+++ 2_spark_streaming.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split, col, length
 from pyspark.sql.types import StructType, StructField, StringType
 
-# Inisiasi Spark Session
+# 1. Inisiasi Spark Session (Sangat Bersih)
 spark = SparkSession.builder \
     .appName("WikiStreamingAnalytics") \
     .getOrCreate()
@@ -15,14 +15,14 @@ schema = StructType([
     StructField("text", StringType(), True)
 ])
 
-# Kembali menggunakan path /app/ karena akan dieksekusi di dalam container
+# 2. Path menggunakan /app/
 df_stream = spark.readStream.schema(schema).json("/app/streaming_data")
 
-# Transformasi Data
+# 3. Transformasi Data
 words = df_stream.select(explode(split(col("text"), "\\s+")).alias("word"))
 word_counts = words.filter(length(col("word")) > 4).groupBy("word").count()
 
-# Tampilkan hasil
+# 4. Tampilkan hasil
 query = word_counts.writeStream \
     .outputMode("complete") \
     .format("console") \
