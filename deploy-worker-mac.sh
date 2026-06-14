2b13121dab0f1f48e1c646b31851038bbefc0d6f
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-06-15 06:35:29 +0800

    feat: update deployment script to use static WORKER_IP and improve network setup

--- deploy-worker-mac.sh
+++ deploy-worker-mac.sh
@@ -1,7 +1,27 @@
+# Buat network baru dengan subnet yang sama dengan WiFi
+podman network create \
+  --driver bridge \
+  --subnet 192.168.1.0/24 \
+  --gateway 192.168.1.1 \
+  spark-net
+
+# Jalankan worker dengan network tersebut
 MASTER_IP=192.168.1.93
-WORKER_IP=$(ipconfig getifaddr en0)
+WORKER_IP=192.168.1.212
 
 podman rm -f datanode spark-worker
+
+podman run -d \
+  --name spark-worker \
+  --network spark-net \
+  --ip $WORKER_IP \
+  -p 7078:7078 \
+  -p 8081:8081 \
+  -e SPARK_MODE=worker \
+  -e SPARK_MASTER_URL=spark://$MASTER_IP:7077 \
+  -e SPARK_WORKER_HOST=$WORKER_IP \
+  -e SPARK_WORKER_PORT=7078 \
+  docker.io/bitnamilegacy/spark:3.5.1
 
 podman run -d \
   --name datanode \
@@ -13,14 +33,3 @@ podman run -d \
   -e CORE_CONF_fs_defaultFS=hdfs://$MASTER_IP:9000 \
   -e HDFS_CONF_dfs_datanode_hostname=$WORKER_IP \
   bde2020/hadoop-datanode:2.0.0-hadoop3.2.1-java8
-
-podman run -d \
-  --name spark-worker \
-  -p 7078:7078 \
-  -p 8081:8081 \
-  -e SPARK_MODE=worker \
-  -e SPARK_MASTER_URL=spark://$MASTER_IP:7077 \
-  -e SPARK_WORKER_HOST=$WORKER_IP \
-  -e SPARK_WORKER_PORT=7078 \
-  -e SPARK_DAEMON_JAVA_OPTS="-Dspark.worker.host=$WORKER_IP" \
-  docker.io/bitnamilegacy/spark:3.5.1
